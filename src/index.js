95a598bce77f456b0bca3b06f2dfaa2033b00349
Svetlana Fedorova <impari.online@yandex.ru>
2021-08-22 19:33:51 +0300

    init: start task typical array problems

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 
 exports.min = function min (array) {
+  let minimum;
+  return minimum = (array.length >= 1) ? Math.min.apply(null, array) : 0;
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maximum;
+  return maxmum = (array.length >= 1) ? Math.max.apply(null, array) : 0;
 }
 
 exports.avg = function avg (array) {
